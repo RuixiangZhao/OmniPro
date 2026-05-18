e69c2a9a13ea3a7340bd2eaf0e4dddbb1f2b6944
zrx <ruixiangzhao@ruc.edu.cn>
2026-05-18 22:17:06 +0800

    init github page

--- metrics/online/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Online-mode metrics."""
-from .scorer import (  # noqa: F401
-    TASK_CONTENT_KIND,
-    match_emits_to_gt,
-    evaluate_sample,
-    aggregate,
-    _score_content,
-    _gt_trigger_sec,
-)
